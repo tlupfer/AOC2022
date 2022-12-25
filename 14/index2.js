02255f2cc917b39d267510a374f603fa43b9bd58
Tim Lupfer <tim@uscreative.works>
2022-12-24 21:36:14 -0600

    Day 14, part two

---
+++ 14/index2.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+
+const lines = fs.readFileSync("input.txt").toString().trim().split('\n');
+const coordinates = lines.map(line => line.match(/\d+,\d+/g)).flat()
+  .map(pair => pair.split(',').map(Number));
+const xMax = Math.max(...coordinates.map(pair => pair[0]));
+const xMin = Math.min(...coordinates.map(pair => pair[0]));
+const yMax = Math.max(...coordinates.map(pair => pair[1]));
+const yMin = 0;
+const xAxis = xMax - xMin + 1001;
+const yAxis = yMax - yMin + 3;
+const grid = [...Array(yAxis).keys()].map(() => [...Array(xAxis).keys()].map(() => '.'));
+grid[grid.length - 1] = [...Array(xAxis).keys()].map(() => '#');
+
+lines.forEach((line) => {
+  const points = line.match(/\d+,\d+/g).map(p => p.split(',').map(Number));
+  for (let i = 0; i < points.length; i++) {
+    const [x, y] = points[i];
+    const xIndex = x - xMin + 500;
+    grid[y][xIndex] = '#';
+    if (points[i - 1]) {
+      const [lastX, lastY] = points[i - 1];
+      const lastXindex = lastX - xMin + 500;
+      if (lastXindex !== xIndex) {
+        const start = lastXindex < xIndex ? lastXindex : xIndex;
+        const finish = lastXindex < xIndex ? xIndex : lastXindex;
+        for (let j = start; j <= finish; j++) grid[y][j] = '#';
+      } else if (lastY !== y) {
+        const start = lastY < y ? lastY : y;
+        const finish = lastY < y ? y : lastY;
+        for (let j = start; j <= finish; j++) {
+          grid[j][xIndex] = '#';
+        }
+      }
+    }
+  }
+});
+
+const isAir = (x, y) => grid[y] && grid[y][x] === '.';
+
+const dropSand = () => {
+  let x = 500 - xMin + 500;
+  if (grid[0][x] === 'O') return;
+  let y = grid.findIndex(row => !isAir(x, row)) - 1;
+  while (true) {
+    if (!x || y === grid.length - 1) break;
+    if (isAir(x, y + 1)) {
+      y++;
+    } else if (isAir(x - 1, y + 1)) {
+      x--;
+      y++;
+    } else if (isAir(x + 1, y + 1)) {
+      x++;
+      y++;
+    } else {
+      grid[y][x] = 'O';
+      break;
+    }
+  }
+};
+
+const getTotal = () => grid.reduce((count, row) => count + row.filter(c => c === 'O').length, 0);
+
+let lastTotal = 0;
+while (true) {
+  dropSand();
+  const newTotal = getTotal();
+  if (newTotal === lastTotal) break;
+  lastTotal = newTotal;
+}
+
+grid.forEach((l) => {
+  l.forEach(c => process.stdout.write(c));
+  process.stdout.write("\n");
+});
+
+console.log(getTotal());
